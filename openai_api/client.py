38f35229368ed60caa01f1c8e0c0184be9010a72
Diogo Marcone <diogo.marcone@hotmail.com>
2024-07-19 10:00:31 -0300

    change api key openai and resources

--- openai_api/client.py
+++ openai_api/client.py
@@ -1,8 +1,19 @@
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("API Key not found. Please set the OPENAI_API_KEY environment variable.")
+
 
 client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY")
+    api_key=api_key
 )
 
 
